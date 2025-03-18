76dcc98b4158dd403ac6209110476c5030d86e79
Amir Amiri <amiriamirr533@gmail.com>
2025-03-18 01:47:28 -0600

    admins added

--- apps/ads/admin.py
+++ apps/ads/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from .models import Ad, AdSet, GlobalAdPricing, Brand, Campaign
 
-# Register your models here.
+
+@admin.register(GlobalAdPricing)
+class GlobalAdPricingAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'cost_per_click', 'cost_per_impression', 'cost_per_view', 'cost_per_acquisition')
+    search_fields = ('uuid',)
+    ordering = ('uuid',)
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'name', 'daily_budget', 'monthly_budget', 'timezone_str', 'owner', 'is_active')
+    list_filter = ('is_active',)
+    search_fields = ('name', 'owner__username')
+    ordering = ('name',)
+
+
+@admin.register(Campaign)
+class CampaignAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'name', 'brand', 'status', 'allowed_start_hour', 'allowed_end_hour')
+    list_filter = ('status', 'brand')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(AdSet)
+class AdSetAdmin(admin.ModelAdmin):
+    list_display = ('uuid', 'name', 'campaign', 'is_active')
+    list_filter = ('campaign', 'is_active')
+    search_fields = ('name',)
+    ordering = ('name',)
+
+
+@admin.register(Ad)
+class AdAdmin(admin.ModelAdmin):
+    list_display = (
+        'uuid',
+        'name',
+        'adset',
+        'is_active',
+        'cost_per_click',
+        'cost_per_impression',
+        'cost_per_view',
+        'cost_per_acquisition'
+    )
+    list_filter = ('adset', 'is_active')
+    search_fields = ('name',)
+    ordering = ('name',)
--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Transaction
 
-# Register your models here.
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        'uuid',
+        'brand',
+        'campaign',
+        'ad',
+        'amount',
+        'transaction_type',
+        'cost_type',
+        'created_at'
+    )
+    list_filter = ('transaction_type', 'cost_type', 'brand')
+    search_fields = (
+        'brand__name',
+        'campaign__name',
+        'ad__name'
+    )
+    ordering = ('-created_at',)
